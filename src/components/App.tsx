178879728bb877069e146d370ba0b069a2557f92
Anastasia C <clarkanastasia@outlook.com>
2024-02-02 13:13:54 +0000

    initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import '../css/App.css'
+import {PhotoViewer} from './PhotoViewer';
+
+export function App() {
+  return (
+    <div className= "flexContainer">
+      <h1>React Photo Viewer</h1>
+      <PhotoViewer />
+      </div>
+  )
+}
---
+++ src/components/PhotoViewer.tsx
@@ -0,0 +1,9 @@
+import '../css/PhotoViewer.css.css'
+
+export function PhotoViewer(){
+    return(
+        <div>
+            <img src="https://picsum.photos/id/237/1000/600" alt="placeholder cover image" className="coverImg"/>
+        </div>
+    );
+}
